3d28742c09db30d1370828ac142ecb3e2a86f7ab
yuelin.ye <yuelin.ye@xbongbong.com>
2022-04-17 15:43:13 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
-
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+<script setup lang="ts">
+import Navbar from './components/Navigation/Navbar.vue';
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 </script>
 
+<template>
+  <div min-h-100vh overflow-hidden block relative pb-30 mx-auto flex flex-col>
+    <Navbar />
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/Navigation/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+<template>
+  <nav
+    z-10 text-elucidator-700 dark:text-dark-repulser-400 relative h-20 px-4
+    role="navigation"
+    aria-label="navbar"
+  >
+  
+  </nav>
+</template>
